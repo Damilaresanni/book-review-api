2632e70226f7dc8e302e618ca90b6ca8b5b59cfe
Damilare sanni <sannidamilare9@gmail.com>
2025-05-28 13:01:01 +0100

    fix: configuring setup

--- Book_review_api_backend/urls.py
+++ Book_review_api_backend/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.http import JsonResponse
+
+def root_view(request):
+    return JsonResponse({"message": "Welcome to the Book Reviews API"})
 
 urlpatterns = [
+    path('', root_view),
     path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls'))
+
 ]
---
+++ config/settings/prod.py
@@ -0,0 +1,12 @@
+from .base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['bookreviews']
+
+
+
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
